911740c8619ebb4253e1fbb8459fb89723b9b1d7
Duba <feyikunmi.oshodi@sisgroup.sg>
2020-12-17 20:48:06 +0800

    Docker Support Refactoring

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,12 @@
 require('dotenv/config'); // load everything from `.env` file into the `process.env` variable
 
-const { DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
 
 module.exports = [{
  name: 'default',
  type: 'mysql',
- host: 'localhost',
- port: DB_PORT,
+ host: DB_HOST || 'localhost',
+ port: DB_PORT || 3306,
  username: DB_USERNAME,
  password: DB_PASSWORD,
  database: DB_DATABASE,
